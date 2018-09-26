14f456ec6255198c69db0544107f6fe9f02ec088
limingliang <limingliang@limingliang-PC>
2018-09-26 13:21:05 +0800

    路由分发配置中心

--- gateway-api/src/main/java/com/green/filter/MyFilter.java
+++ gateway-api/src/main/java/com/green/filter/MyFilter.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.netflix.zuul.ZuulFilter;
@@ -14,6 +15,9 @@ import com.netflix.zuul.exception.ZuulException;
 public class MyFilter extends ZuulFilter{
 	
 	private static Logger log = LoggerFactory.getLogger(MyFilter.class);
+	
+	@Value("${server.port}")
+	String port ;
 	
 	@Override
 	public Object run() throws ZuulException {
