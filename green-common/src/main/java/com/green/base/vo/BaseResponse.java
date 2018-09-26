ec6b69eec4095e878455210a62a9da1326100fe7
limingliang <limingliang@limingliang-PC>
2018-09-26 15:46:40 +0800

    添加公共模块，引入admin中

---
+++ green-common/src/main/java/com/green/base/vo/BaseResponse.java
@@ -0,0 +1,57 @@
+package com.green.base.vo;
+
+import com.green.enums.HttpStatusEnum;
+
+public class BaseResponse<E> {
+
+	private int status = HttpStatusEnum.OK.getStatus();
+	private String message = HttpStatusEnum.OK.getMessage();
+	E result;
+
+	public BaseResponse(int statusCode, String message, E result) {
+		this.status = statusCode;
+		this.message = message;
+		this.result = result;
+	}
+
+	public BaseResponse(int statusCode, E result) {
+		this.status = statusCode;
+		this.message = HttpStatusEnum.getMessage(statusCode);
+		this.result = result;
+	}
+
+	public BaseResponse(int statusCode) {
+		this(statusCode, null);
+	}
+
+	public BaseResponse(E result) {
+		this.result = result;
+	}
+
+	public BaseResponse() {
+	}
+
+	public E getResult() {
+		return result;
+	}
+
+	public void setResult(E result) {
+		this.result = result;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
---
+++ green-common/src/main/java/com/green/enums/HttpStatusEnum.java
@@ -0,0 +1,31 @@
+package com.green.enums;
+
+public enum HttpStatusEnum {
+	OK(200, "操作成功"), INVALID_REQUEST(400, "参数错误"), UNAUTHORIZED(401, "没有权限"), FORBIDDEN(403, "禁止访问"), NOT_FOUND(404,
+			"资源不存在");
+
+	private final int status;
+	private final String message;
+
+	public static String getMessage(int status) {
+		for (HttpStatusEnum c : HttpStatusEnum.values()) {
+			if (c.getStatus() == status) {
+				return c.getMessage();
+			}
+		}
+		return null;
+	}
+
+	HttpStatusEnum(final int status, final String message) {
+		this.status = status;
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+}
