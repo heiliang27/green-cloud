de2878cf5bf0ec0aef32e658c4f636a65ef1aaef
limingliang <limingliang@limingliang-PC>
2018-09-26 15:10:12 +0800

    添加web支持

--- green-common/src/main/java/com/green/base/controller/BaseController.java
+++ green-common/src/main/java/com/green/base/controller/BaseController.java
@@ -1,5 +1,14 @@
 package com.green.base.controller;
 
-public class BaseController {
+import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BaseController {
+	@Autowired
+	private HttpServletRequest request;
+	
+	protected org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(this.getClass());
+	
+	
 }
