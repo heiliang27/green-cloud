445a0d544c00bbbab98d6105621f221ab6c34699
limingliang <limingliang@limingliang-PC>
2018-09-26 14:07:51 +0800

    修改gateway支持引入config-clent

--- gateway-api/src/main/java/com/green/filter/MyFilter.java
+++ gateway-api/src/main/java/com/green/filter/MyFilter.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.netflix.zuul.ZuulFilter;
@@ -15,9 +14,6 @@ import com.netflix.zuul.exception.ZuulException;
 public class MyFilter extends ZuulFilter{
 	
 	private static Logger log = LoggerFactory.getLogger(MyFilter.class);
-	
-	@Value("${server.port}")
-	String port ;
 	
 	@Override
 	public Object run() throws ZuulException {
