6987bc3a67e938fc114d3e9ec5f362055393944e
limingliang <limingliang@limingliang-PC>
2018-10-26 17:18:03 +0800

    使用spring-data-redis

--- green-admin/src/main/java/com/green/common/config/RedisConfig.java
+++ green-admin/src/main/java/com/green/common/config/RedisConfig.java
@@ -3,7 +3,14 @@ package com.green.common.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.green.bean.properties.RedisProperties;
 
 import redis.clients.jedis.JedisPool;
@@ -11,7 +18,6 @@ import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
 public class RedisConfig {
-
 	@Autowired
 	private RedisProperties redisProperties;
 
@@ -19,16 +25,51 @@ public class RedisConfig {
 	@Bean
 	public JedisPoolConfig jedisPoolConfig() {
 		JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
-		redisProperties.jedisPoolConfig(jedisPoolConfig);
+		RedisProperties.Pool pool = this.redisProperties.getJedis().getPool();
+		if (pool != null) {
+			jedisPoolConfig = jedisPoolConfig(pool);
+		}
 		System.out.println("jedisPoolConfig--初始化----------------------------");
 		return jedisPoolConfig;
 	}
 
 	/** JedisPool连接池 */
 	@Bean
-	public JedisPool jedisPool() {
-		JedisPool jedisPool = redisProperties.jedisPool(jedisPoolConfig());
+	public JedisPool jedisPool(JedisPoolConfig jedisPoolConfig) {
+		JedisPool jedisPool = new JedisPool(jedisPoolConfig, redisProperties.getHost(), redisProperties.getPort(),
+				(int)redisProperties.getTimeout().toMillis());
 		System.out.println("jedisPool--初始化----------------------------");
 		return jedisPool;
+	}
+
+	@Bean
+	public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+		// 使用Jackson2JsonRedisSerialize 替换默认序列化
+		Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(
+				Object.class);
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+		objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+		jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+		// 设置value的序列化规则和 key的序列化规则
+		redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.afterPropertiesSet();
+		return redisTemplate;
+	}
+
+	private JedisPoolConfig jedisPoolConfig(RedisProperties.Pool pool) {
+		JedisPoolConfig config = new JedisPoolConfig();
+		config.setMaxTotal(pool.getMaxActive());
+		config.setMaxIdle(pool.getMaxIdle());
+		config.setMinIdle(pool.getMinIdle());
+		if (pool.getMaxWait() != null) {
+			config.setMaxWaitMillis(pool.getMaxWait().toMillis());
+		}
+		return config;
 	}
 }
--- green-commons/src/main/java/com/green/bean/properties/RedisProperties.java
+++ green-commons/src/main/java/com/green/bean/properties/RedisProperties.java
@@ -1,119 +1,232 @@
 package com.green.bean.properties;
 
+import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
-import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
-
-@Component
-@ConfigurationProperties(prefix = "redis")
+@Configuration
+@ConfigurationProperties(prefix = "spring.redis")
 public class RedisProperties {
 
-	private int maxTotal = 8;
+	/**
+	 * Database index used by the connection factory.
+	 */
+	private int database = 0;
 
-	private int maxIdle = 8;
+	/**
+	 * Connection URL. Overrides host, port, and password. User is ignored.
+	 * Example: redis://user:password@example.com:6379
+	 */
+	private String url;
 
-	private int minIdle = 0;
+	/**
+	 * Redis server host.
+	 */
+	private String host = "localhost";
 
-	private long maxWaitMillis = -1L;
+	/**
+	 * Login password of the redis server.
+	 */
+	private String password;
 
-	private String host = "127.0.0.1";
+	/**
+	 * Redis server port.
+	 */
+	private int port = 6379;
 
-	private Integer port = 6379;
+	/**
+	 * Whether to enable SSL support.
+	 */
+	private boolean ssl;
 
-	private String password = "123456";
+	/**
+	 * Connection timeout.
+	 */
+	private Duration timeout;
 
-	private Integer timeout = 2000;
+	private final Jedis jedis = new Jedis();
 
-	private Integer database = 0;
+	private final Lettuce lettuce = new Lettuce();
 
-	public void jedisPoolConfig(JedisPoolConfig jedisPoolConfig) {
-
-		jedisPoolConfig.setMinIdle(minIdle);
-
-		jedisPoolConfig.setMaxIdle(maxIdle);
-		// 连接池的最大数据库连接数
-		jedisPoolConfig.setMaxTotal(maxTotal);
-		// 最大建立连接等待时间
-		jedisPoolConfig.setMaxWaitMillis(maxWaitMillis);
+	public int getDatabase() {
+		return this.database;
 	}
 
-	public JedisPool jedisPool(JedisPoolConfig jedisPoolConfig) {
-		JedisPool jedisPool = new JedisPool(jedisPoolConfig, host, port, timeout, password, database);
-		return jedisPool;
+	public void setDatabase(int database) {
+		this.database = database;
 	}
 
-	public int getMaxTotal() {
-		return maxTotal;
+	public String getUrl() {
+		return this.url;
 	}
 
-	public void setMaxTotal(int maxTotal) {
-		this.maxTotal = maxTotal;
-	}
-
-	public int getMaxIdle() {
-		return maxIdle;
-	}
-
-	public void setMaxIdle(int maxIdle) {
-		this.maxIdle = maxIdle;
-	}
-
-	public int getMinIdle() {
-		return minIdle;
-	}
-
-	public void setMinIdle(int minIdle) {
-		this.minIdle = minIdle;
-	}
-
-	public long getMaxWaitMillis() {
-		return maxWaitMillis;
-	}
-
-	public void setMaxWaitMillis(long maxWaitMillis) {
-		this.maxWaitMillis = maxWaitMillis;
+	public void setUrl(String url) {
+		this.url = url;
 	}
 
 	public String getHost() {
-		return host;
+		return this.host;
 	}
 
 	public void setHost(String host) {
 		this.host = host;
 	}
 
-	public Integer getPort() {
-		return port;
-	}
-
-	public void setPort(Integer port) {
-		this.port = port;
-	}
-
 	public String getPassword() {
-		return password;
+		return this.password;
 	}
 
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-	public Integer getTimeout() {
-		return timeout;
+	public int getPort() {
+		return this.port;
 	}
 
-	public void setTimeout(Integer timeout) {
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public boolean isSsl() {
+		return this.ssl;
+	}
+
+	public void setSsl(boolean ssl) {
+		this.ssl = ssl;
+	}
+
+	public void setTimeout(Duration timeout) {
 		this.timeout = timeout;
 	}
 
-	public Integer getDatabase() {
-		return database;
+	public Duration getTimeout() {
+		return this.timeout;
 	}
 
-	public void setDatabase(Integer database) {
-		this.database = database;
+	public Jedis getJedis() {
+		return this.jedis;
 	}
-	
+
+	public Lettuce getLettuce() {
+		return this.lettuce;
+	}
+
+	/**
+	 * Pool properties.
+	 */
+	public static class Pool {
+
+		/**
+		 * Maximum number of "idle" connections in the pool. Use a negative
+		 * value to indicate an unlimited number of idle connections.
+		 */
+		private int maxIdle = 8;
+
+		/**
+		 * Target for the minimum number of idle connections to maintain in the
+		 * pool. This setting only has an effect if it is positive.
+		 */
+		private int minIdle = 0;
+
+		/**
+		 * Maximum number of connections that can be allocated by the pool at a
+		 * given time. Use a negative value for no limit.
+		 */
+		private int maxActive = 8;
+
+		/**
+		 * Maximum amount of time a connection allocation should block before
+		 * throwing an exception when the pool is exhausted. Use a negative
+		 * value to block indefinitely.
+		 */
+		private Duration maxWait = Duration.ofMillis(-1);
+
+		public int getMaxIdle() {
+			return this.maxIdle;
+		}
+
+		public void setMaxIdle(int maxIdle) {
+			this.maxIdle = maxIdle;
+		}
+
+		public int getMinIdle() {
+			return this.minIdle;
+		}
+
+		public void setMinIdle(int minIdle) {
+			this.minIdle = minIdle;
+		}
+
+		public int getMaxActive() {
+			return this.maxActive;
+		}
+
+		public void setMaxActive(int maxActive) {
+			this.maxActive = maxActive;
+		}
+
+		public Duration getMaxWait() {
+			return this.maxWait;
+		}
+
+		public void setMaxWait(Duration maxWait) {
+			this.maxWait = maxWait;
+		}
+
+	}
+
+	/**
+	 * Jedis client properties.
+	 */
+	public static class Jedis {
+
+		/**
+		 * Jedis pool configuration.
+		 */
+		private Pool pool;
+
+		public Pool getPool() {
+			return this.pool;
+		}
+
+		public void setPool(Pool pool) {
+			this.pool = pool;
+		}
+
+	}
+
+	/**
+	 * Lettuce client properties.
+	 */
+	public static class Lettuce {
+
+		/**
+		 * Shutdown timeout.
+		 */
+		private Duration shutdownTimeout = Duration.ofMillis(100);
+
+		/**
+		 * Lettuce pool configuration.
+		 */
+		private Pool pool;
+
+		public Duration getShutdownTimeout() {
+			return this.shutdownTimeout;
+		}
+
+		public void setShutdownTimeout(Duration shutdownTimeout) {
+			this.shutdownTimeout = shutdownTimeout;
+		}
+
+		public Pool getPool() {
+			return this.pool;
+		}
+
+		public void setPool(Pool pool) {
+			this.pool = pool;
+		}
+
+	}
+
 }
