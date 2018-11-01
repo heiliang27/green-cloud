6cfc368749a1ea8404a2c7fb5a3d218c875cde9e
limingliang <limingliang@limingliang-PC>
2018-11-01 17:44:22 +0800

    添加测试url

--- green-admin/src/main/java/com/green/modular/admin/controller/UserController.java
+++ green-admin/src/main/java/com/green/modular/admin/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.green.modular.admin.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
 import com.green.base.controller.BaseController;
+import com.green.base.vo.BaseResponse;
+import com.green.modular.admin.entity.User;
 
 /**
  * <p>
@@ -17,5 +20,17 @@ import com.green.base.controller.BaseController;
 @RestController
 @RequestMapping("/admin/user")
 public class UserController extends BaseController {
+	
+	@Value("server.port")
+	String port;
+	
+	@RequestMapping("/list")
+	public BaseResponse<User> getUserList(String name){
+		User user = new User();
+		user.setAccount("mmmm");
+		user.setName("limingli9ang");
+		user.setEmail(port);
+		return new BaseResponse<User>(user);
+	}
 
 }
